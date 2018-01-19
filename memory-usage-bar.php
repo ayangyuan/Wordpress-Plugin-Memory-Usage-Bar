e73cbbaacd6513c27609a243c06ff16337215172
root <ayangyuan@gmail.com>
2018-01-18 17:26:20 -0800

    1.0.1

--- memory-usage-bar.php
+++ memory-usage-bar.php
@@ -1,22 +1,35 @@
 <?php
-/*
-Plugin Name: Memory Usage Bar 
-Plugin URI: https://wordpress.org/plugins/memory-usage-bar/
-Github: https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar.git 
-Description: Show the current memory usage in the admin header.
-Version: 1.0.1 
-Author: Yuan Yang
-Author URI: https://84361749.com
-Text Domain: memory-usage-bar
-
-*/
+/**
+ * Memory Usage Bar 
+ *
+ * Memory Usage Bar is a powerful plugin for WordPress that allows you to display current memory usage on top of the admin header.  It so useful to monitor the total memory useage for a single visit.
+ *
+ * @package   Memory Usage Bar
+ * @author    Mr.ING <ayangyuan@gmail.com>
+ * @license   GPL-2.0+
+ * @link      https://squaredaway.studio/wordpress-plugin-memory-usage-bar/
+ * @copyright 1999-2018 
+ *
+ * @wordpress-plugin
+ * Plugin Name: Memory Usage Bar
+ * Plugin URI:  https://wordpress.org/plugins/memory-usage-bar/
+ * GitHub URI:  https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar
+ * Author URI:  https://squaredaway.studio/wordpress-plugin-memory-usage-bar/
+ * Author:      Mr.ING 
+ * Version:     1.0.1
+ * Text Domain: memory-usage-bar
+ * Domain Path: /res/lang
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Description: Display the current memory usage bar on admin header.
+ */
 
 if ( is_admin() ) {	
-    add_filter( 'admin_bar_menu', 'MUB_add_header' ,990);
-    function MUB_add_header() {
+    add_filter( 'admin_bar_menu', 'add_header' ,990);
+    function add_header($admin_bar) {
         $admin_bar->add_menu(array(
              'id'=>'memory_usage_bar',
-             'title'=>MUB_memory_usage_bar(),
+             'title'=>memory_usage_bar(),
         ));
         $admin_bar->add_menu(array(
              'parent'=>'memory_usage_bar',
@@ -26,12 +39,12 @@ if ( is_admin() ) {
         $admin_bar->add_menu(array(
              'parent'=>'memory_usage_bar',
              'id'=>'memory_usage_wp_limit',
-             'title'=>MUB_memory_usage_wp_limit(),
+             'title'=>memory_usage_wp_limit(),
         ));
         $admin_bar->add_menu(array(
              'parent'=>'memory_usage_bar',
              'id'=>'memory_usage_ip',
-             'title'=>MUB_memory_usage_ip(),
+             'title'=>memory_usage_ip(),
         ));
         $admin_bar->add_menu(array(
              'parent'=>'memory_usage_bar',
@@ -40,7 +53,7 @@ if ( is_admin() ) {
         ));
     }
 
-    function MUB_memory_usage_bar() {
+    function memory_usage_bar() {
         $usage = function_exists('memory_get_peak_usage') ? round(memory_get_peak_usage(TRUE) / 1024 / 1024, 2) : 0;                                            
         $limit = (int) ini_get('memory_limit') ;
         if ( !empty($usage) && !empty($limit) ) 
@@ -49,7 +62,7 @@ if ( is_admin() ) {
         return $content;
 
     }
-    function MUB_memory_usage_wp_limit(){
+    function memory_usage_wp_limit(){
         $unit  = substr( WP_MEMORY_LIMIT, -1 );
         $limit = substr( WP_MEMORY_LIMIT, 0, -1 );
         $limit = (int)$limit;  
@@ -58,11 +71,34 @@ if ( is_admin() ) {
         $content = "WP Limit: $memory";
         return $content;
     }
-    function MUB_memory_usage_ip(){
+    function memory_usage_ip(){
          $server_ip_address = (!empty($_SERVER[ 'SERVER_ADDR' ]) ? $_SERVER[ 'SERVER_ADDR' ] : "");
          if ($server_ip_address == "") 
               $server_ip_address = (!empty($_SERVER[ 'LOCAL_ADDR' ]) ? $_SERVER[ 'LOCAL_ADDR' ] : "");
          $content = ' IP:  ' . $server_ip_address . '';
          return $content;
     }
+
+
+/**
+ * Add links to the plugin action row.
+ */
+if ( ! defined( 'MR_ING_MUB_PLUGIN_FILE' ) ) {define( 'MR_ING_MUB_PLUGIN_FILE', __FILE__ );}
+function mr_ing_mub_plugin_actions( $links, $file ) {
+
+        if ( plugin_basename( MR_ING_MUB_PLUGIN_FILE ) === $file ) {
+
+                $new_links = array(
+                        'support'    => '<a href = "http://wordpress.org/support/plugin/memory_usage_bar">' . __( 'Support' ) . '</a>',
+                        'donate'     => '<a href = "https://squaredaway.studio/donate/">' . __( 'Donate') . '</a>',
+                        'contribute' => '<a href = "https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar">' . __( 'Contribute' ) . '</a>',
+                );
+
+                $links = array_merge( $links, $new_links );
+        }
+        return $links;
+}
+add_filter( 'plugin_row_meta', 'mr_ing_mub_plugin_actions', 10, 2 );
+
+
 }
--- readme.txt
+++ readme.txt
@@ -1,23 +1,76 @@
 === Memory Usage Bar ===
-Contributors: Yuan Yang 
-Author URI: https://84361749.com/
+Contributors: Mr.ING
+Author URI: https://squaredaway.studio/wordpress-plugin-memory-usage-bar/ 
 Plugin URI: https://wordpress.org/plugins/memory-usage-bar/
-Github: https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar.git
-Tags: memory, memory-limit,  server, info
-Requires at least: 1.0.1
-Tested up to: 4.9.1
+Github: https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar
+Donate link: https://squaredaway.studio/donate
+Tags: memory, memory-limit, server, info, admin, ip, memory limit
+Stable tag: trunk
+Tested up to: 4.9
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Show the current memory usage bar on admin header.
+Display the current memory usage bar on admin header.
 
 == Description ==
 
-This plugin displays the current memory usage bar on the admin header.
+[Memory Usage Bar](https://squaredaway.studio/wordpress-plugin-memory-usage-bar/) is a powerful plugin for WordPress that allows you to display current memory usage on top of the admin header. It so useful to monitor the total memory useage for a single visit.
 
-You can easily see in the admin header the current memory usage, no more scoll down to the footer.
+= Key features =
 
-Plug, Activate, and Forget. 
+* **Visible**: At the header, its so easy to monitor the current usage.
+* **IAF**: This plug is prefer to work sliently, that once you Installed, Activated, then you will Forget it exist.
+* **Green**: Nothing added to your Wordpress but only pure function.
 
-A super lite plugin with out updating very now and them, simply does it job, and that's it.
+= Donations =
+
+Would you like to buy me a cup of coffee, like friends hang out at Starbucks someday in the afternoon.
+
+= Contribute =
+
+This plug is also available on [Github](https://github.com/ayangyuan/Wordpress-Plugin-Memory-Usage-Bar).
+So, if you've got some cool feature that you'd like to implement into the plugin or a bug you've been able to fix, consider forking the project and sending me a pull request.
+
+== Installation ==
+
+= WordPress install (The easy way) =
+
+1. Navigate to Plugins within your WordPress Admin Area
+
+2. Click "Add new" and in the search box enter "Memory Usage Bar"
+
+3. Find the plugin in the list and click "Install Now"
+
+= Manual install =
+
+1. Download the plugin
+
+2. Extract the contents of .zip to wp-content/plugins/ folder.
+
+3. Activate the Plugin in WP-Admin under the Plugins screen
+
+
+== Screenshots ==
+
+1. It shows the length of your posts, and it also be able to sort.
+2. If you can't find it or you want to hidden it temporarily, go and uncheck it
+
+== Frequently Asked Questions ==
+
+Any issues regarding to this pluging? feel free to ask me at the [WordPress.org support forum](http://wordpress.org/support/plugin/memory-usage-bar)
+
+= What is the first question gonna be like? =
+I have no idea.
+
+== Changelog ==
+
+= 1.0.1 =
+
+* Released:
+        * Add screenshot, logo and banner.
+        * Write readme.txt
+
+= 1.0.0 =
+
+* Initialized.
 
